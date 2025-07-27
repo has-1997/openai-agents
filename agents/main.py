d89a14a55e17f6738910a109a8d93e43bf4d5043
has-1997 <scammaps@protonmail.com>
2025-07-27 21:33:24 +0100

    Update main.py

--- agents/main.py
+++ agents/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from agents import Agent, ModelSettings, function_tool
+from pydantic import BaseModel
 
 load_dotenv()
 
@@ -15,8 +16,18 @@ agent = Agent(
     tools=[get_weather],
 )
 
+class CalendarEvent(BaseModel):
+    name: str
+    date: str
+    participants: list[str]
 
+calendar_extractor_agent = Agent(
+    name="Calendar Extractor Agent",
+    instructions="Extract calendar events from a given text",
+    model="gpt-4o-mini",
+    output_type=CalendarEvent,
+)
 
-
-
-
+calendar_agent = Agent(
+    name="Calendar Agent",
+    instructions="Create a calendar event based on the user's request",
